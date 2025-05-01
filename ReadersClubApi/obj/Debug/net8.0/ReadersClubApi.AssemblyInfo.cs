da253378163ad1143654a6d518cf1c9205731a2c
naghoma12 <naghamy473@gmail.com>
2025-05-01 18:55:42 +0300

    updates

--- ReadersClubApi/obj/Debug/net8.0/ReadersClubApi.AssemblyInfo.cs
+++ ReadersClubApi/obj/Debug/net8.0/ReadersClubApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadersClubApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c212a590c1d0e65235253e69f76396d44a910f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63515099177e0dfcbd3c7d923a991181b66c1445")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadersClubApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadersClubApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
